f7b32a5d53a510c635a726597da296e8280df5b8
Thibaut MICETIC <thibaut.micetic@orange.fr>
2024-11-28 11:09:36 +0100

    part Espace utilisateut done

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -12,6 +12,67 @@
                     {{ __("You're logged in!") }}
                 </div>
             </div>
+
+            <!-- Message flash -->
+            @if (session('error'))
+                <div class="bg-red-500 text-white p-4 rounded-lg mt-6 mb-6 text-center">
+                    {{ session('error') }}
+                </div>
+            @endif
+
+            <!-- Message flash -->
+            @if (session('insertSuccess'))
+                <div class="bg-green-500 text-white p-4 rounded-lg mt-6 mb-6 text-center">
+                    {{ session('insertSuccess') }}
+                </div>
+            @endif
+
+            <!-- Message flash -->
+            @if (session('updateSuccess'))
+                <div class="bg-green-500 text-white p-4 rounded-lg mt-6 mb-6 text-center">
+                    {{ session('updateSuccess') }}
+                </div>
+            @endif
+
+            <!-- Message flash -->
+            @if (session('deleteSuccess'))
+                <div class="bg-green-500 text-white p-4 rounded-lg mt-6 mb-6 text-center">
+                    {{ session('deleteSuccess') }}
+                </div>
+            @endif
+
+            <!-- Articles -->
+            @if($articles->count() > 0)
+            @foreach ($articles as $article)
+                <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg mt-4">
+                    <div class="p-6 text-gray-900">
+                        <h2 class="text-2xl font-bold">{{ $article->title }}</h2>
+                        <p class="text-gray-700">{{ substr($article->content, 0, 30) }}...</p>
+                        <div class="d-flex flex-row">
+                            <div class="text-right">
+                                <a href="{{ route('articles.edit', $article->id) }}" class="text-gray-600 hover:text-gray-950">Modifier</a>
+                            </div>
+                            <form method="post" action="{{ route('articles.remove', $article) }}" class="mt-2">
+                                @csrf
+                                @method('delete')
+
+                                <div class="flex justify-end">
+                                    <x-danger-button class="ms-3">
+                                        {{ __('Supprimer') }}
+                                    </x-danger-button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+                @else
+                <div class="mt-6 bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        {{ __("Vous n'avez pas créé d'articles.") }}
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </x-app-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('/dashboard', [UserController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    // Articles
+    Route::get('/articles/create', [UserController::class, 'create'])->name('articles.create');
+    Route::post('/articles/store', [UserController::class, 'store'])->name('articles.store');
+    Route::get('/articles/{article}/edit', [UserController::class, 'edit'])->name('articles.edit');
+    Route::post('/articles/{article}/update', [UserController::class, 'update'])->name('articles.update');
+    Route::delete('/articles/{article}/delete', [UserController::class, 'remove'])->name('articles.remove');
 });
 
 require __DIR__.'/auth.php';
