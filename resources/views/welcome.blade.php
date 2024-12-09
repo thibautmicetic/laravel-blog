134da3b5f7a5894f2d3ac5ed8fe248a54d57aa52
Admin <Admin@PC1>
2024-12-09 09:54:01 +0100

    add likes to prod

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -80,6 +80,7 @@
                                                     @endforeach
                                                 </div>
                                                 <p class="text-gray-700">{{ substr($article->content, 0, 30) }}...</p>
+                                                <p class="text-gray-700">Nombre de likes: {{ $article->likes }}</p>
                                             </div>
                                         </div>
                                         <a href="{{ route('public.show', [$article->user_id, $article->id]) }}" class="block bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded text-center">
